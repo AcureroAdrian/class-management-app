391a0f5687cc7f062e8ac528ad8aac6ce1389ad0
Martin Angulo <martin.angulo@devnavigate.com>
2025-02-23 11:26:47 -0500

    remove safeAreaView in screens

--- screens/control-panel/SettingsScreen/SettingsScreen.tsx
+++ screens/control-panel/SettingsScreen/SettingsScreen.tsx
@@ -5,7 +5,6 @@ import { USER_LOGOUT } from '@/redux/constants/userConstants'
 import ScreenHeader from '@/components/ScreenHeader/ScreenHeader'
 import capitalizeWords from '@/shared/capitalize-words'
 import { TUserRole } from '@/shared/common-types'
-import { SafeAreaViewStyled } from '@/theme/styles'
 
 const SettingsScreen = ({ role }: { role: TUserRole }) => {
 	const dispatch = useAppDispatch()
@@ -17,16 +16,14 @@ const SettingsScreen = ({ role }: { role: TUserRole }) => {
 	}
 
 	return (
-		<SafeAreaViewStyled>
-			<View style={{ flex: 1 }}>
-				<ScreenHeader label='Settings' />
-				<View style={{ gap: 10, marginTop: 20, marginBottom: 20, padding: 20 }}>
-					<Text>Name: {capitalizeWords(userInfo?.name || '')}</Text>
-					<Text>Last Name: {capitalizeWords(userInfo?.lastName || '')}</Text>
-					<Button title='Logout' onPress={handleOnLogout} />
-				</View>
+		<View style={{ flex: 1 }}>
+			<ScreenHeader label='Settings' />
+			<View style={{ gap: 10, marginTop: 20, marginBottom: 20, padding: 20 }}>
+				<Text>Name: {capitalizeWords(userInfo?.name || '')}</Text>
+				<Text>Last Name: {capitalizeWords(userInfo?.lastName || '')}</Text>
+				<Button title='Logout' onPress={handleOnLogout} />
 			</View>
-		</SafeAreaViewStyled>
+		</View>
 	)
 }
 
