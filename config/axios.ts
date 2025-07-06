e5fd624dec81630a468c7445e4735d075266e591
Adrian Acurero <adrianacurero@gmail.com>
2025-07-05 23:35:10 -0500

    Actualizar la configuración de Axios para reducir el tiempo de espera a 6000 ms y añadir un interceptor de respuesta que maneje errores de tiempo de espera, proporcionando un mensaje más amigable al usuario. Esto mejora la gestión de errores en las solicitudes a la API.

--- config/axios.ts
+++ config/axios.ts
@@ -2,12 +2,22 @@ import axios from 'axios'
 
 const customAxios = axios.create({
 	baseURL: process.env.EXPO_PUBLIC_API_URL,
-	timeout: 10000,
+	timeout: 6000,
 })
 
 customAxios.interceptors.request.use(config => {
 	config.headers['x-api-key'] = process.env.EXPO_PUBLIC_API_KEY
 	return config
 })
+
+customAxios.interceptors.response.use(
+	(response) => response,
+	(error) => {
+		if (error.code === 'ECONNABORTED' || error.message.includes('timeout')) {
+			error.message = 'The server is taking too long to respond. Please try again in a minute.'
+		}
+		return Promise.reject(error)
+	}
+)
 
 export default customAxios
